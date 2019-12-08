956981a504f21bf2e0d92f8d815bd8a5d7291226
msuga <m.sugano0416@gmail.com>
2019-12-08 11:01:21 +0900

    ファーストコミット

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy : Token
+{
+
+    //アニメーション用のスプライト
+    public Sprite spr0;
+
+    public Sprite spr1;
+
+    int tAnim = 0;
+
+    void FixedUpdate()
+    {
+        tAnim++;
+        if (tAnim % 32 < 16)
+        {
+            SetSprite(spr0);
+        }
+        else
+        {
+            SetSprite(spr1);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Field : Token
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //マップ読み込み
+        TMXLoader tmx = new TMXLoader();
+        tmx.Load("Levels/map");
+
+        //経路レイヤーを取得
+        Layer2D path = tmx.GetLayer("path");
+        //デバッグ出力
+        path.Dump();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
